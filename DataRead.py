231d59988ac0bd591082629435b686c84d8144cd
tkyuvu <54690377+kieutrinh326@gmail.com>
2019-11-21 16:27:27 -0500

    Data cleaning and some plots
    Remove duplicates and NAs
    Plot counts for some variables

--- DataRead.py
+++ DataRead.py
@@ -23,14 +23,108 @@ userdata = pd.read_csv(filepath2)
 #%% [markdown]
 #Trinh test commmit
 import os
-# dirpath = os.getcwd() 
-# print("current directory is : " + dirpath)
+import pandas as pd
+dirpath = os.getcwd() 
+#print("current directory is : " + dirpath)
 # path2add = ''
-filepath1 ='C:/Users/karti/OneDrive/Documents/6103Project/googleplaystore.csv'
+filepath1 ='googleplaystore.csv'
 pdata = pd.read_csv(filepath1)
 
-filepath2 = 'C:/Users/karti/OneDrive/Documents/6103Project/googleplaystore_user_reviews.csv'
+filepath2 = 'googleplaystore_user_reviews.csv'
 userdata = pd.read_csv(filepath2)
+
+#%%
+# Standard quick checks
+def dfChkBasics(dframe): 
+  cnt = 1  
+  try:
+    print(str(cnt)+': info(): ')
+    print(dframe.info())
+  except: pass
+
+  cnt+=1
+  print(str(cnt)+': describe(): ')
+  print(dframe.describe())
+
+  cnt+=1
+  print(str(cnt)+': dtypes: ')
+  print(dframe.dtypes)
+
+  try:
+    cnt+=1
+    print(str(cnt)+': columns: ')
+    print(dframe.columns)
+  except: pass
+
+  cnt+=1
+  print(str(cnt)+': head() -- ')
+  print(dframe.head())
+
+  cnt+=1
+  print(str(cnt)+': shape: ')
+  print(dframe.shape)
+
+  # cnt+=1
+  # print(str(cnt)+': columns.value_counts(): ')
+  # print(dframe.columns.value_counts())
+
+def dfChkValueCnts(dframe):
+  cnt = 1
+  for i in dframe.columns :
+    print(str(cnt)+':', i, 'value_counts(): ')
+    print(dframe[i].value_counts())
+    cnt +=1
+#%%
+# Do a quick check
+dfChkBasics(pdata)
+dfChkValueCnts(pdata)
+
+#%%[markdown]
+# The dataset has 10841 rows and 13 columns. There are duplicates in App names. Let's drop duplicates.
+
+#%%
+pdatac = pdata.drop_duplicates(subset=['App'], keep = 'first')
+print(pdatac.shape)
+print(pdatac.info())
+
+#%%[markdown]
+# There are NA values in Rating, Type, and Content Rating. Let's remove them
+
+pdata_clean = pdatac.dropna()
+pdata_clean.info()
+
+# We have 8190 rows now.
+
+#%%[markdown]
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Plot counts for each Category
+plt.figure(figsize=(20,5))
+fig = sns.countplot(x=pdata_clean['Category'], palette="hls")
+fig.set_xticklabels(fig.get_xticklabels(),rotation=90)
+plt.show(fig)
+
+#%%[markdown]
+# Barplot for Catergory and Rating
+plt.figure(figsize=(8,6))
+fig = sns.barplot(x=pdata_clean['Category'], y=pdata_clean['Rating'], palette="hls")
+fig.set_xticklabels(fig.get_xticklabels(),rotation=90)
+plt.tight_layout()
+plt.show(fig)
+
+#%%[markdown]
+# Plot counts for each Type
+fig = sns.countplot(x=pdata_clean['Type'], palette="hls")
+fig.set_xticklabels(fig.get_xticklabels(),rotation=90)
+plt.show(fig)
+
+#%%[markdown]
+# Plot counts for Installs
+plt.figure(figsize=(20,5))
+fig = sns.countplot(x=pdata_clean['Installs'], palette="hls")
+fig.set_xticklabels(fig.get_xticklabels(),rotation=90)
+plt.show(fig)
 
 #%% [markdown]
 #Ayush
