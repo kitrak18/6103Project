a21ce61bc8892e26d70a71e3b97f9d422987a45f
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2019-12-04 15:15:37 -0500

    tried kmeans with knn data

--- Regressor .py
+++ Regressor .py
@@ -399,6 +399,43 @@ for i, weights in enumerate(['uniform']):
 
 plt.tight_layout()
 plt.show()
+
+
+
+#%%
+from sklearn.cluster import KMeans
+
+# kmeans = KMeans(n_clusters=2)
+# kmeans.fit(X_train)
+
+X_train1 = pd.concat([X_train,y_train],axis=1)
+
+xdata = X_train1[['Rating', 'Reviews', 'Installs', 'Price','Size','Genres','Type']]
+#%%
+
+km_xdata = KMeans( n_clusters=3, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0 )
+y_km = km_xdata.fit_predict(xdata)
+
+# plot the 3 clusters
+index1 = 4#5 #4 
+index2 = 0#0
+
+plt.scatter( xdata[y_km==0].iloc[:,index1], xdata[y_km==0].iloc[:,index2], s=25, c='lightgreen', marker='s', edgecolor='black', label='cluster 1' )
+
+plt.scatter( xdata[y_km==1].iloc[:,index1], xdata[y_km==1].iloc[:,index2], s=25, c='orange', marker='o', edgecolor='black', label='cluster 2' )
+
+plt.scatter( xdata[y_km==2].iloc[:,index1], xdata[y_km==2].iloc[:,index2], s=25, c='lightblue', marker='v', edgecolor='black', label='cluster 3',alpha=0.1)
+
+# plot the centroids
+plt.scatter( km_xdata.cluster_centers_[:, index1], km_xdata.cluster_centers_[:, index2], s=250, marker='*', c='red', edgecolor='black', label='centroids' )
+plt.legend(scatterpoints=1)
+plt.xlabel(xdata.columns[index1])
+plt.ylabel(xdata.columns[index2])
+plt.grid()
+plt.show()
+
+#%%
+
 # %%[markdown]
 
 # ### Random Forest Model
